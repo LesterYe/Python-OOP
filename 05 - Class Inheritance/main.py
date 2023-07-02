37251b644cb308865c317335fb7410f9a86c3a22
LesterYe <48324429+LesterYe@users.noreply.github.com>
2023-07-02 21:17:14 +0400

    Finished tutorial on class inheritance

--- 05 - Class Inheritance/main.py
+++ 05 - Class Inheritance/main.py
@@ -53,7 +53,42 @@ class Item:
       return False
 
   def __repr__(self):
-    return f'Item("{self.name}", {self.price}, {self.quantity})'
+    # return f'Item("{self.name}", {self.price}, {self.quantity})'
+    return f'{self.__class__.__name__}("{self.name}", {self.price}, {self.quantity})'
 
-# Item.instantiate_from_csv()
-# print(Item.all)
+class Phone(Item):
+  # all = [] # this is inherited by Item
+  def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
+    # Call to super function to have access to all attributes/methods
+    # , including class attributes/methods
+    super().__init__(
+      name, price, quantity
+    )
+    
+    # # Run validations to the received arguments
+    # assert price >= 0, f'Price {price} is not greater than or equal to zero!'
+    # assert quantity >= 0, f'Quantity {quantity} is not greater than or equal to zero!'
+    assert broken_phones >= 0, f'Broken Phones {broken_phones} is not greater than or equal to zero!'
+
+    # # Assign to self object
+    # self.name = name
+    # self.price = price
+    # self.quantity = quantity
+    self.broken_phones = broken_phones
+
+    # # Actions to execute
+    # Phone.all.append(self)
+
+  def __repr__(self):
+    # return f'Item("{self.name}", {self.price}, {self.quantity})'
+    return f'{self.__class__.__name__}("{self.name}", {self.price}, {self.quantity}, {self.broken_phones})'
+
+
+phone1 = Phone("jscPhonev10", 500, 5, 1)
+# phone1.broken_phones = 1
+# phone2 = Phone("jscPhonev20", 700, 5, 1)
+# phone2.broken_phones = 1
+
+# print(phone1.calculate_total_price())
+print(Item.all)
+print(Phone.all)
